826363e2a7abc152d1dd6b236ecaa175ce05d27d
priosshrsth <shrsthprios@hotmail.com>
2021-05-28 13:49:12 +0545

    added editor config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,6 @@ module.exports = {
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
 	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2019
-	},
 	env: {
 		browser: true,
 		es2017: true,
